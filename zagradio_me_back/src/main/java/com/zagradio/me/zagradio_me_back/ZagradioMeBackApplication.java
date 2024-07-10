0cf06c6d7943d76f44e973858afd74bd0d530e78
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2024-07-10 19:52:19 +0200

    initial

---
+++ zagradio_me_back/src/main/java/com/zagradio/me/zagradio_me_back/ZagradioMeBackApplication.java
@@ -0,0 +1,13 @@
+package com.zagradio.me.zagradio_me_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZagradioMeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZagradioMeBackApplication.class, args);
+	}
+
+}
---
+++ zagradio_me_back/src/test/java/com/zagradio/me/zagradio_me_back/ZagradioMeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zagradio.me.zagradio_me_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZagradioMeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

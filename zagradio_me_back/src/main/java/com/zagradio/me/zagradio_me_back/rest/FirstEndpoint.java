2e387247c2a5f1606672a711fa6abeb23630b83d
arslansmajevic <iamarslanb@gmail.com>
2024-07-11 16:45:19 +0200

    chore: adding logging on backend

--- zagradio_me_back/src/main/java/com/zagradio/me/zagradio_me_back/rest/FirstEndpoint.java
+++ zagradio_me_back/src/main/java/com/zagradio/me/zagradio_me_back/rest/FirstEndpoint.java
@@ -1,17 +1,24 @@
 package com.zagradio.me.zagradio_me_back.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.invoke.MethodHandles;
+
 @RestController
 @RequestMapping(path = "test")
 public class FirstEndpoint {
 
+    private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     @GetMapping
     public ResponseEntity<String> firstEndpointMethod() {
+        log.trace("got a request");
         return new ResponseEntity<>("Arslan was here", HttpStatus.OK);
     }
 }

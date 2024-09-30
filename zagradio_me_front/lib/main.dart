e90334b4d2b852da98d82f65bfe273ac059be188
amarderschrecklicher <115952128+amarderschrecklicher@users.noreply.github.com>
2024-09-30 14:07:12 +0200

    api

--- zagradio_me_front/lib/main.dart
+++ zagradio_me_front/lib/main.dart
@@ -6,6 +6,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 Future<void> main() async {
   
   await dotenv.load(fileName: ".env"); 
+  
+  String apiKey = dotenv.env['API_KEY'] ?? '';
 
   runApp(const MyApp());
 }
